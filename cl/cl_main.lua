9728b4e20b81433100f381b6c1f0d025b72e80ac
AAspect <68797946+AAspect@users.noreply.github.com>
2021-09-17 20:28:08 -0500

    fix(anchor): Vehicle Checks

--- cl/cl_main.lua
+++ cl/cl_main.lua
@@ -1,15 +1,17 @@
 
 RegisterCommand('anchor', function()
-    local ped = PlayerPedId();
+    local ped = PlayerPedId()
     local veh = GetVehiclePedIsIn(ped, true)
-    if IsPedOnVehicle(ped) and vehicleClass == 14 and (model ~= "SUBMERS" and model ~= "SEASHARK" and model ~= "SEASHARK2" and model ~= "SEASHARK3" and model ~= "SUBMERS2")  then
+    local vehicleClass = GetVehicleClass(veh)
+    local model = GetDisplayNameFromVehicleModel(GetEntityModel(GetVehiclePedIsIn(ped)))
+    if IsPedOnVehicle(ped) and vehicleClass == 14 and (model ~= "SUBMERS" and model ~= "SEASHARK" and model ~= "SEASHARK2" and model ~= "SEASHARK3" and model ~= "SUBMERS2") then
         TriggerServerEvent('anchor:server:toggleState', NetworkGetNetworkIdFromEntity(veh))
+    end
 end)
 
 RegisterNetEvent('client:toggleAnchor', function(state, ent)
     local ped = PlayerPedId();
     local veh = GetVehiclePedIsIn(ped, true)
-    local vehicleClass = GetVehicleClass(vehicle)
     local vehposition = GetEntityCoords(veh)
     if state == 'anchor' then 
         SetBoatAnchor(veh, true)
